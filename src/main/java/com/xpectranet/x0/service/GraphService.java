e5cd82d420f115c5ff4edb5249e4ec8d41263b59
XpectraNet <dev@xpectradata.com>
2025-04-03 15:07:43 +0100

    Delete src/main/java/com/xpectranet/x0 directory

--- src/main/java/com/xpectranet/x0/service/GraphService.java
+++
@@ -1,8 +0,0 @@
-package com.xpectranet.x0.service;
-
-/**
- * GraphService defines the interface for traversing the insight graph.
- */
-public interface GraphService {
-    String traverse(String tag, String layer);
-}
